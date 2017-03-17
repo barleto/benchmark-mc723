2d773e31057814e04ccc127bd2f2e5664160eae2
lacavazan <lucascpds@gmail.com>
2017-03-16 21:31:00 -0300

    windows nao eh mais uma opcao

--- youtube-dl-benchmark.py
+++ youtube-dl-benchmark.py
@@ -3,18 +3,18 @@ import os
 import subprocess
 import time
 
-if "-w" in sys.argv:
-    osFlag = "W"
-    initialTime = time.time()
-    subprocess.call(["youtube-dl.exe", "https://www.youtube.com/watch?v=5q9gk5_-4fY", "-o", "arquivoVideo"])
-elif "-u" in sys.argv:
+# if "-w" in sys.argv:
+#     osFlag = "W"
+#     initialTime = time.time()
+#     subprocess.call(["youtube-dl.exe", "https://www.youtube.com/watch?v=5q9gk5_-4fY", "-o", "arquivoVideo"])
+# elif "-u" in sys.argv:
     subprocess.call(["chmod", "a+rx", "./youtube-dl"])
     osFlag = "U"
     initialTime = time.time()
     subprocess.call(["youtube-dl", "https://www.youtube.com/watch?v=5q9gk5_-4fY", "-o", "arquivoVideo"])
-else:
-     print "Please specify your kind of OS with flag -w (windows) or -u (unix base systems)"
-     sys.exit()
+# else:
+#      print "Please specify your kind of OS with flag -w (windows) or -u (unix base systems)"
+#      sys.exit()
 
 endTime = time.time()
 
