c71779b42a130a514c2e87ac4d8d2bfe497c4879
Ricardo Charf <riczcharf@gmail.com>
2017-05-18 20:37:55 -0300

    python test script

--- proj2/mips-2.4.0/execute.py
+++ proj2/mips-2.4.0/execute.py
@@ -1,5 +1,20 @@
+#!/usr/bin/python3
 import subprocess
-stdoutdata = subprocess.getoutput("./runMips.sh")
+
+def findError(strOut,err):
+    if strOut.find("Error")!=-1 or strOut.find("error")!=-1 or strOut.find("ERROR")!=-1:
+        print("ERROR FOUND:")
+        print(strOut)
+        raise Exception("Error found when building!",err)
+
+stdoutdata = subprocess.getoutput("acsim mips.ac -abi")
+findError(stdoutdata,"acsim")
+stdoutdata = subprocess.getoutput("make")
+findError(stdoutdata,"make")
+stdoutdata = subprocess.getoutput("mips-newlib-elf-gcc -specs=archc hello2.c -o hello2.mips")
+findError(stdoutdata,"mips-gcc")
+stdoutdata = subprocess.getoutput("./mips.x --load=hello2.mips")
+findError(stdoutdata,"mips.x")
 report = []
 flag = False
 for line in stdoutdata.split("\n"):
