0594b9a3e0c299fa67919906af22b2b30792f8d9
Ricardo Charf <riczcharf@gmail.com>
2017-05-18 20:52:35 -0300

    script now config too

--- proj2/mips-2.4.0/execute.py
+++ proj2/mips-2.4.0/execute.py
@@ -1,5 +1,41 @@
 #!/usr/bin/python3
 import subprocess
+import sys
+
+sys.argv = sys.argv[1:]
+
+if(len(sys.argv)!=5):
+    print("Usage: ./execute.py programName USING_FOWARDING	IS_SUPERESCALAR	isPredictorActive pipeLineSize")
+    quit()
+else:
+    configFileTemplate = """//Here we put all variables and MACROS that control processor configuration
+
+//Cache trace config
+string programName = "{}";
+bool generateTraces = true;//generate traces for later use in DineroIV
+
+//Data hazard config
+#define USING_FOWARDING	{}
+
+//Superscalar config
+#define IS_SUPERESCALAR	{}
+
+//Control Hazard config
+bool isPredictorActive = {};
+
+//Pipeline Size
+int pipeLineSize = {};
+""".format(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+    # Open the file with writing permission
+    myfile = open("configMIPS.h", 'w')
+
+    # Write a line to the file
+    myfile.write(configFileTemplate)
+
+    # Close the file
+    myfile.close()
+
 
 def findError(strOut,err):
     if strOut.find("Error")!=-1 or strOut.find("error")!=-1 or strOut.find("ERROR")!=-1:
